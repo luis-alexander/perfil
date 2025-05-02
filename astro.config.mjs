548efed3ee11af6f847ba2ddcb8474513e26ae02
luis-alexander <luis.soto@sotsun.com>
2025-05-01 20:11:38 -0500

    Deplo astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://luis-alexander.github.io',
-  base: '/perfil-web',
+  base: '/perfil',
 });
